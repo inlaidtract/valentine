1f63689f4aff2deb060afe169140d0d9cae01dd4
inlaidtract <125136891+inlaidtract@users.noreply.github.com>
2023-02-13 09:37:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -305,8 +305,8 @@ function reveal() {
 
     var ifrm = document.createElement("iframe");
     ifrm.setAttribute("src", "https://www.youtube.com/embed/MqazV4hbu8E?autoplay=1");
-    ifrm.style.width = `${288}px`;
-    ifrm.style.height = `${191}px`;
+//     ifrm.style.width = `${288}px`;
+//     ifrm.style.height = `${191}px`;
     ifrm.style.border = 'none';
     document.querySelector('#video').appendChild(ifrm);
 
